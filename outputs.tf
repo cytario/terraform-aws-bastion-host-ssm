6aaee241ec8fd76df24f6bfea776a4a036325f8d
Martin Schulze <martin@slash-m.com>
2025-08-13 10:57:44 +0200

    feat: add security_group_id to outputs

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@ output "instance_arn" {
   value       = join("", aws_instance.this[*].arn)
   description = "Bastion host instance ARN"
 }
+
+output "security_group_id" {
+  value       = aws_security_group.this.id
+  description = "ID of bastion host's security group "
+}
